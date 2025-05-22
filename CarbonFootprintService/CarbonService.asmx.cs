09bd33bf9905e09d243425164ae9ce8d5f3084e3
Administrator <Administrator@SELAB8-013>
2025-05-22 13:26:16 +0500

    Add project files.

---
+++ CarbonFootprintService/CarbonService.asmx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CarbonFootprintService
+{
+  
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class CarbonService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public double CalculateEnergyFootprint(double energyKWh, double emissionFactor)
+        {
+            return energyKWh * emissionFactor;
+        }
+
+        [WebMethod]
+        public double CalculateTransportFootprint(double distanceKm, double emissionFactor)
+        {
+            return distanceKm * emissionFactor;
+        }
+
+        [WebMethod]
+        public double CalculateWasteFootprint(double wasteTonnes, double emissionFactor)
+        {
+            return wasteTonnes * emissionFactor;
+        }
+
+        [WebMethod]
+        public double CalculateTotalFootprint(double energyFootprint, double transportFootprint, double wasteFootprint)
+        {
+            return energyFootprint + transportFootprint + wasteFootprint;
+        }
+    }
+}
